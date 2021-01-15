320e306a16f53db1e6849132c7b3a341f377af4f
vladimirovsk <vladimirovsk@gmail.com>
2021-01-15 10:41:12 +0100

    main

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
 import Main from './component/Main';
 import Login from './component/Login';
 import Profile from './component/Profile';
+
 
 function App () {
     
     return (
       <div>
-      <ul>
-        <li><Link to='/'>MAIN</Link></li>
-        <li><Link to='/login'>LOGIN</Link></li>
-        <li><Link to='/profile'>PROFILE</Link></li>
-      </ul>
+        <Navbar bg="light" variant="light">
+          
+            <Nav className="mr-auto">
+            <Nav.Link href="/">Main</Nav.Link>
+            <Nav.Link href="/Login">Login</Nav.Link>
+            <Nav.Link href="/Profile">Profile</Nav.Link>
+          </Nav>
+      </Navbar>
 
       <Switch>
           <Route exact path="/">
